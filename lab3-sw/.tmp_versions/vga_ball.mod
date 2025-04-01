f8330ddd5df97194f799c25880e25ce2ba98d531
RobiyeW <robelwkh@gmail.com>
2025-04-01 22:14:44 +0000

    First Commit with working foundation

---
+++ lab3-sw/.tmp_versions/vga_ball.mod
@@ -0,0 +1,3 @@
+/root/Lab3-EM/lab3-sw/vga_ball.ko
+/root/Lab3-EM/lab3-sw/vga_ball.o
+
---
+++ lab3-sw/.vga_ball.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/root/Lab3-EM/lab3-sw/vga_ball.ko := ld -r  -EL -T ./scripts/module-common.lds -T ./arch/arm/kernel/module.lds  --build-id  -o /root/Lab3-EM/lab3-sw/vga_ball.ko /root/Lab3-EM/lab3-sw/vga_ball.o /root/Lab3-EM/lab3-sw/vga_ball.mod.o ;  true
