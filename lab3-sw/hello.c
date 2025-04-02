0159642624729cb18a158be4cfe7648985fbc22e
FITD-fash <ztm2106@columbia.edu>
2025-04-01 23:07:24 -0400

    Update hello.c

--- lab3-sw/hello.c
+++ lab3-sw/hello.c
@@ -11,7 +11,7 @@
 #define BOX_WIDTH 640
 #define BOX_HEIGHT 480
 #define FRAME_TIME_MICROSECONDS 16666   // ~60 FPS (VGA sync)
-#define FRAME_SKIP 1                    // Update ball every 5 frames
+#define FRAME_SKIP 10                    // Update ball every 5 frames
 
 int vga_ball_fd;
 
