40f5027d620fb57277884d85c7aca6b0999db259
FITD-fash <ztm2106@columbia.edu>
2025-04-01 20:11:20 -0400

    1st

--- lab3-sw/vga_ball.h
+++ lab3-sw/vga_ball.h
@@ -2,14 +2,20 @@
 #define _VGA_BALL_H
 
 #include <linux/ioctl.h>
+#include <stdint.h>
 
 typedef struct {
   unsigned char red, green, blue;
 } vga_ball_color_t;
-  
+
+typedef struct {
+  uint16_t x;
+  uint16_t y;
+} vga_ball_position_t;
 
 typedef struct {
   vga_ball_color_t background;
+  vga_ball_position_t position;
 } vga_ball_arg_t;
 
 #define VGA_BALL_MAGIC 'q'
@@ -17,5 +23,6 @@ typedef struct {
 /* ioctls and their arguments */
 #define VGA_BALL_WRITE_BACKGROUND _IOW(VGA_BALL_MAGIC, 1, vga_ball_arg_t)
 #define VGA_BALL_READ_BACKGROUND  _IOR(VGA_BALL_MAGIC, 2, vga_ball_arg_t)
+#define VGA_BALL_WRITE_POSITION   _IOW(VGA_BALL_MAGIC, 3, vga_ball_arg_t)
 
 #endif
